0673188165174eb35d023c910b26c8102dc32261
= <=>
2019-10-03 17:58:59 -0500

    added logic to take in and parse file

--- MazeProblem/MazeProblem/MazeProblemMain.cs
+++ MazeProblem/MazeProblem/MazeProblemMain.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MazeProblem.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace MazeProblem
 {
@@ -6,7 +10,40 @@ namespace MazeProblem
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            if (!args.Any())
+                Console.WriteLine("No definition file was given. Please enter the path to the definition file and run again.");
+
+            var definitionFilePath = args[0];
+
+            if (!File.Exists(definitionFilePath))
+                Console.WriteLine($"No file exists for {definitionFilePath}.");
+
+            var definitionFile = ReadDefinitionFile(definitionFilePath);
+
+
+
+        }
+
+        private static DefinitionFile ReadDefinitionFile(string definitionFilePath)
+        {
+            var definitionFileContents = File.ReadAllLines(definitionFilePath);
+
+            // TODO: Check for valid file contents
+
+            var definitionFile = new DefinitionFile
+            {
+                BoardSize = definitionFileContents[0],
+                MirrorPlacements = new List<String>(),
+                LazerEntryRoom = definitionFileContents[definitionFileContents.Length - 2]
+            };
+
+            for (int i = 2; i < definitionFileContents.Length - 3; i++)
+            {
+                definitionFile.MirrorPlacements.Add(definitionFileContents[i]);
+            }
+
+            return definitionFile;
         }
     }
 }
---
+++ MazeProblem/MazeProblem/Models/Maze.cs
@@ -0,0 +1,24 @@
+﻿namespace MazeProblem.Models
+{
+    public class Maze
+    {
+        #region "Member Variables"
+        private int _height;
+        private int _width;
+        #endregion
+
+        #region "Properties"
+        public int Height 
+        { 
+            get => _height;
+            set { _height = value >= 0 ? value : 0; }
+        }
+
+        public int Width 
+        { 
+            get => _width;
+            set { _width = value >= 0 ? value : 0; }
+        }
+        #endregion
+    }
+}
