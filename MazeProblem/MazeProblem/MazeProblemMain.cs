6701dafd9847fecb3284a992b5d8d8945d5321db
= <=>
2019-10-03 18:23:54 -0500

    started on create maze logic

--- MazeProblem/MazeProblem/MazeProblemMain.cs
+++ MazeProblem/MazeProblem/MazeProblemMain.cs
@@ -12,12 +12,18 @@ namespace MazeProblem
         {
 
             if (!args.Any())
+            {
                 Console.WriteLine("No definition file was given. Please enter the path to the definition file and run again.");
+                Environment.Exit(0);
+            }
 
-            var definitionFilePath = args[0];
+            var definitionFilePath = args[0].Trim();
 
             if (!File.Exists(definitionFilePath))
+            {
                 Console.WriteLine($"No file exists for {definitionFilePath}.");
+                Environment.Exit(0);
+            }
 
             var definitionFile = ReadDefinitionFile(definitionFilePath);
 
@@ -33,17 +39,31 @@ namespace MazeProblem
 
             var definitionFile = new DefinitionFile
             {
-                BoardSize = definitionFileContents[0],
+                BoardSize = definitionFileContents[0].Trim(),
                 MirrorPlacements = new List<String>(),
-                LazerEntryRoom = definitionFileContents[definitionFileContents.Length - 2]
+                LazerEntryRoom = definitionFileContents[definitionFileContents.Length - 2].Trim()
             };
 
             for (int i = 2; i < definitionFileContents.Length - 3; i++)
             {
-                definitionFile.MirrorPlacements.Add(definitionFileContents[i]);
+                definitionFile.MirrorPlacements.Add(definitionFileContents[i].Trim());
             }
 
             return definitionFile;
+        }
+
+        private static Maze CreateMaze(DefinitionFile definitionFile)
+        {
+            var maze = new Maze();
+
+            var mazeWidthAndHeight = definitionFile.BoardSize.Split(',');
+
+            maze.Width = Int32.Parse(mazeWidthAndHeight[0]);
+            maze.Height = Int32.Parse(mazeWidthAndHeight[1]);
+
+            // TODO
+
+            return maze;
         }
     }
 }
