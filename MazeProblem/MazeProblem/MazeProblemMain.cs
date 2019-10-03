5a6dc9f860737905eb0244d44650dd339f86031c
= <=>
2019-10-03 16:38:41 -0500

    Init

---
+++ MazeProblem/MazeProblem/MazeProblemMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MazeProblem
+{
+    public class MazeProblemMain
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
